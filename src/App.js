0b1a11a7379e1c0b10cfe86d1601f98a9d00590d
hajjimajed <hajjimajed78@gmail.com>
2023-01-09 13:37:42 +0100

    creating the navbar and home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-function App() {
+import { Routes, Route } from 'react-router-dom'
+
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+
+const App = () => {
   return (
-    <div>
-      hello world
-    </div>
+
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+
+        <Route index element={<Home />} />
+
+      </Route>
+    </Routes>
+
   );
 }
 
